bc22b89c8e67203be7ff10dd363b3d57932822e3
Екатерина Лепесевич <13958@mail.ru>
2022-07-07 16:10:49 +0300

    Understanding Undefined Value returned from a Function

---
+++ 56.js
@@ -0,0 +1,17 @@
+// Setup
+let sum = 0;
+
+function addFive() {
+  sum = sum + 5;
+}
+function addThree(){
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+
+// Only change code above this line
+
+addThree();
+addFive();
