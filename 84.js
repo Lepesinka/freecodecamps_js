93c7c8ab1f18947a5dfc75318f38f77b09ca2e22
Екатерина Лепесевич <13958@mail.ru>
2022-10-04 11:59:58 +0300

    Build JavaScript Objects

---
+++ 84.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Tusik",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Bobik", "Lodik"]
+    // Only change code above this line
+  };
